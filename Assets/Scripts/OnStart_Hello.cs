e23193fc2ffd6689fb833d24c7ad7e2e87e408c1
Owen Mundy <omundy@gmail.com>
2022-11-06 08:29:25 -0500

    Adds basic scripts to test

---
+++ Assets/Scripts/OnStart_Hello.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnStart_Hello : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log(StaticTest.greeting);
+    }
+
+}
