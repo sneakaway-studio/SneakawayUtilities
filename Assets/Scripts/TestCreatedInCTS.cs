82f492288e55ee5e8542bdcdb47e4d2d7aaf7983
Owen Mundy <omundy@gmail.com>
2022-11-06 08:39:55 -0500

    Edits from CTS

---
+++ Assets/Scripts/TestCreatedInCTS.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCreatedInCTS : MonoBehaviour
+{
+	void Start()
+    {
+        Debug.Log("hello from CTS-created script");
+    }
+
+}
