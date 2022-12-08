d99b7d29b9d727826b3b59f238eefd59f34d3854
Owen Mundy <omundy@gmail.com>
2022-12-08 11:10:53 -0500

    Update serialization notes

--- Assets/Scripts/Serialization/SaveInspectorValues.cs
+++ Assets/Scripts/Serialization/SaveInspectorValues.cs
@@ -2,32 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using SneakawayUtilities;
+using UnityEngine.Serialization;
 
 /**
  *  Move old serialized values to new properties
- *  - Add everything below (except the class) and 
  */
-
-
-// UPDATE: Maybe better method?
-// https://www.youtube.com/watch?v=Nl2imdYPm38&ab_channel=DanPos-GameDevTutorials%21
-// https://docs.unity3d.com/ScriptReference/Serialization.FormerlySerializedAsAttribute.html
-// 1. Add this, keeping old var in: [FormerlySerializedAs("fieldName")] newFieldName = new List<Field>();
-// 2. Switch to the Editor (to let it serialize)
-// Now you can remove the old one
 
 namespace SneakawayUtilities
 {
     public class SaveInspectorValues : MonoBehaviour
     {
 
-        public int oldProp;
-        public int prop;
+        // METHOD 1 -> FormerlySerializedAs
+        // https://www.youtube.com/watch?v=Nl2imdYPm38&ab_channel=DanPos-GameDevTutorials%21
+        // https://docs.unity3d.com/ScriptReference/Serialization.FormerlySerializedAsAttribute.html
+
+        public List<Object> oldProp = new List<Object>();
+        [FormerlySerializedAs("oldProp")] public List<Object> newFieldName = new List<Object>();
+
+        // 1. Add FormerlySerializedAs attribute
+        // 2. Switch to the Editor (to let it serialize)
+        // 3. Now you can remove the old field
+
+
+
+
+        // METHOD 2 -> OnValidate() is called when script is compiled
 
         void OnValidate()
         {
-            prop = oldProp;
-
             // can also be used to iterate over lists in scriptable objects
             // public List<VizFiles> vizFiles = new List<VizFiles>();
             // foreach (var f in vizFiles)
