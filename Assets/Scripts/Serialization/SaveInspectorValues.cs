23a2528d8941048582e6ed4b270229fbaf634aa9
Owen Mundy <omundy@gmail.com>
2022-12-08 11:05:43 -0500

    Update notes about serialization

--- Assets/Scripts/Serialization/SaveInspectorValues.cs
+++ Assets/Scripts/Serialization/SaveInspectorValues.cs
@@ -5,30 +5,37 @@ using SneakawayUtilities;
 
 /**
  *  Move old serialized values to new properties
- *  - Add everything below (except the class) and switch to the Editor (just once)
+ *  - Add everything below (except the class) and 
  */
+
+
+// UPDATE: Maybe better method?
+// https://www.youtube.com/watch?v=Nl2imdYPm38&ab_channel=DanPos-GameDevTutorials%21
+// https://docs.unity3d.com/ScriptReference/Serialization.FormerlySerializedAsAttribute.html
+// 1. Add this, keeping old var in: [FormerlySerializedAs("fieldName")] newFieldName = new List<Field>();
+// 2. Switch to the Editor (to let it serialize)
+// Now you can remove the old one
 
 namespace SneakawayUtilities
 {
-	// [ExecuteInEditMode]
-	public class SaveInspectorValues : MonoBehaviour
-	{
+    public class SaveInspectorValues : MonoBehaviour
+    {
 
-	    public int oldProp;
-	    public int prop;
+        public int oldProp;
+        public int prop;
 
-	    void OnValidate()
-	    {
-	        prop = oldProp;
+        void OnValidate()
+        {
+            prop = oldProp;
 
-			// can also be used to iterate over lists in scriptable objects
-		    // public List<VizFiles> vizFiles = new List<VizFiles>();
-			// foreach (var f in vizFiles)
-			// {
-			// 	f.prop = f.oldProp;
-			// }
+            // can also be used to iterate over lists in scriptable objects
+            // public List<VizFiles> vizFiles = new List<VizFiles>();
+            // foreach (var f in vizFiles)
+            // {
+            // 	f.prop = f.oldProp;
+            // }
+        }
 
-	    }
 
-	}
+    }
 }
