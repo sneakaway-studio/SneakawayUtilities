b886be114da6e4bc55aa36d0c4c24ae1b3b18eec
Owen Mundy <omundy@gmail.com>
2024-03-23 13:39:05 -0400

    Adds time formats

--- Assets/Scripts/Time/TimeTools.cs
+++ Assets/Scripts/Time/TimeTools.cs
@@ -21,7 +21,6 @@ namespace SneakawayUtilities
             public int prev;
             public int current;
             public int next;
-            //public int nextnext; // OMG
             public int count;
             // set default values
             public Indexer(int _count)
@@ -45,9 +44,6 @@ namespace SneakawayUtilities
                 next = current + 1; // set next
                 if (next >= count) next = 0; // if should loop
 
-                //nextnext = next + 1; // set nextnext
-                //if (nextnext >= count) nextnext = 0; // if should loop
-
                 prev = current - 1; // set prev
                 if (prev < 0) prev = count - 1; // if should loop
             }
@@ -64,6 +60,27 @@ namespace SneakawayUtilities
         public static string ToStringTime24(this TimeSpan dt)
         {
             return dt.ToString(@"HH\:mm\:ss");
+        }
+        public static string ToStringSpanFromSeconds(this int seconds){
+            return TimeSpan.FromSeconds(seconds).ToString(@"hh\:mm\:ss");
+        }
+
+        public static DateTime StringToDateTime(string timeStr, string format = "HH:mm:ss tt")
+        {
+            var result = Convert.ToDateTime(timeStr);
+            //string test = result.ToString(format, CultureInfo.CurrentCulture);
+            return result;
+        }
+
+        public static TimeSpan StringToTimeSpan(string str)
+        {
+            TimeSpan ts;
+
+            if (!TimeSpan.TryParse(str, out ts))
+            {
+                ts = new TimeSpan(0, 1, 0);
+            }
+            return ts;
         }
 
     }
