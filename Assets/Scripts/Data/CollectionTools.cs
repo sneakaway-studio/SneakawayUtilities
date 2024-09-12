155312287524a56ad0ff386c38382199f7e133c6
Owen Mundy <omundy@gmail.com>
2024-09-12 12:31:15 -0400

    Format code

--- Assets/Scripts/Data/CollectionTools.cs
+++ Assets/Scripts/Data/CollectionTools.cs
@@ -4,109 +4,82 @@ using UnityEngine;
 
 namespace SneakawayUtilities
 {
-    public static class CollectionTools
-    {
+	public static class CollectionTools
+	{
 
-        /////////////////////////////////
-        ///////////// ENUM //////////////
-        /////////////////////////////////
 
-		// TODO: Write tests for these enum extensions
 
-    	/// <summary>[Extension] Return enum value as string</summary>
-		public static string AsString<T>(this T value) where T : Enum
+
+		/////////////////////////////////
+		//////////// LIST<T> ////////////
+		/////////////////////////////////
+
+		/// <summary>Find index of list<T> item where field = val (good for data organized by slug)</summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="list"></param>
+		/// <returns>-1 if not found</returns>
+		public static int FindListIndexByPropertyValue<T>(List<T> list, string field, string val)
 		{
-			return Enum.GetName(typeof(T), value);
-		}
-
-		public static T AsEnum<T>(this int value) where T : Enum
-		{
-			if (Enum.IsDefined(typeof(T), value))
+			int _index = -1;
+			for (int i = 0; i < list.Count; i++)
 			{
-				return (T)Enum.ToObject(typeof(T), value);
+				if (Equals(list[i].GetType().GetField(field).GetValue(list[i]), val))
+				{
+					return i;
+				}
 			}
-			else
-			{
-				throw new ArgumentException($"{value} is not a valid value for enum {typeof(T).Name}");
-			}
+			return _index;
+
+
+			//     return list.FindIndex(p =>
+			//         Equals(p.GetType().GetProperty(field).GetValue(list), val));
 		}
-    	/// <summary>[Extension] Return enum value as int</summary>
-		public static int AsInt<T>(this T value) where T : Enum
+
+		/// <summary>Remove (not destroy) all in items from any List<T></summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="list"></param>
+		/// <returns></returns>
+		public static List<T> RemoveAllFromList<T>(List<T> list)
 		{
-			return (int)(object)value;
-		}
-
-
-        /////////////////////////////////
-        //////////// LIST<T> ////////////
-        /////////////////////////////////
-
-        /// <summary>Find index of list<T> item where field = val (good for data organized by slug)</summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="list"></param>
-        /// <returns>-1 if not found</returns>
-        public static int FindListIndexByPropertyValue<T>(List<T> list, string field, string val)
-        {
-            int _index = -1;
-            for (int i = 0; i < list.Count; i++)
-            {
-                if (Equals(list[i].GetType().GetField(field).GetValue(list[i]), val))
-                {
-                    return i;
-                }
-            }
-            return _index;
-
-
-        //     return list.FindIndex(p =>
-        //         Equals(p.GetType().GetProperty(field).GetValue(list), val));
-        }
-
-        /// <summary>Remove (not destroy) all in items from any List<T></summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="list"></param>
-        /// <returns></returns>
-        public static List<T> RemoveAllFromList<T>(List<T> list)
-        {
-			int i = list.Count-1;
+			int i = list.Count - 1;
 			while (i >= 0)
 				list.RemoveAt(i--);
 			return list;
-        }
+		}
 
 
 
-        /// <summary>
+		/// <summary>
 		/// Find a transform in a child (used non-static w/o transform parameter)
 		/// </summary>
 		/// <param name="transform"></param>
 		/// <param name="str"></param>
 		/// <returns></returns>
-        public static Transform FindChildTransformSafe(Transform transform, string str)
-        {
-            try
-            {
-                Transform t = transform.Find(str);
-                if (t != null) return t;
-            }
-            catch (MissingReferenceException e)
-            {
-                Debug.Log(e.Message);
-            }
-            return transform;
-        }
+		public static Transform FindChildTransformSafe(Transform transform, string str)
+		{
+			try
+			{
+				Transform t = transform.Find(str);
+				if (t != null) return t;
+			}
+			catch (MissingReferenceException e)
+			{
+				Debug.Log(e.Message);
+			}
+			return transform;
+		}
 
 
 
-        public static void HideAllInList(List<GameObject> list)
-        {
-            for (int i = 0; i < list.Count; i++)
-            {
-                list[i].gameObject.SetActive(false);
-            }
-        }
+		public static void HideAllInList(List<GameObject> list)
+		{
+			for (int i = 0; i < list.Count; i++)
+			{
+				list[i].gameObject.SetActive(false);
+			}
+		}
 
 
 
-    }
+	}
 }
